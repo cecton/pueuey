360f4f746f0fc913e84c6e4348ffebb0e1d849e6
Cécile Tonglet <cecile.tonglet@gmail.com>
2014-03-26 15:08:57 +0100

    Added: worker tests

---
+++ tests/example_worker.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python2
+
+import argparse
+import os
+import sys
+import time
+import psycopg2
+import logging
+
+import pueuey
+from pueuey import Worker
+
+
+def touch(name, sleep=0):
+    logging.info("touch `%s'", name)
+    fh = open(name, 'wb')
+    fh.close()
+    time.sleep(sleep)
+
+class MyWorker(Worker):
+    def __init__(self, working_directory, **kwargs):
+        self.working_directory = working_directory
+        super(MyWorker, self).__init__(**kwargs)
+
+    def call(self, job):
+        origin_directory = os.getcwd()
+        os.chdir(self.working_directory)
+        try:
+            if self.fork_worker:
+                logging.debug("PID/PPID: %d/%d", os.getpid(), os.getppid())
+            super(MyWorker, self).call(job)
+        finally:
+            os.chdir(origin_directory)
+
+    def handle_failure(self, job, e):
+        super(MyWorker, self).handle_failure(job, e)
+        logging.error("dsn: " + self.conn_adapter.connection.dsn)
+
+parser = argparse.ArgumentParser(add_help=False)
+parser.add_argument('--help', action='store_true')
+parser.add_argument('--dbname', '-d', dest='dbname', type=str)
+parser.add_argument('--host', '-h',
+    help="Specifies the TCP port or the local Unix-domain socket file.")
+parser.add_argument('--username', '-U',
+    help="Connect to the database as the user username instead "
+         "of the default.")
+parser.add_argument('--port', '-p', type=int)
+parser.add_argument('--queue', default='example_worker')
+parser.add_argument('--forkworker', action='store_true', default=None)
+parser.add_argument('--debug', action='store_true')
+parser.add_argument('working_directory', nargs='?',
+    help="Working directory to monitor the events (required)")
+
+def run(args):
+    if args.help or not args.working_directory:
+        parser.print_help()
+        sys.exit(0)
+
+    if args.debug or os.environ.get('DEBUG', ''):
+        logging.basicConfig(level=logging.DEBUG)
+
+    if args.dbname:
+        conn = psycopg2.connect(database=args.dbname,
+            host=args.host, port=args.port, username=args.username)
+    else:
+        conn = None
+
+    worker = MyWorker(args.working_directory,
+        connection=conn, q_name=args.queue, fork_worker=args.forkworker)
+    worker.start()
+    sys.exit(1)
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    run(args)
--- tests/test_30_worker.py
+++ tests/test_30_worker.py
@@ -1,17 +1,113 @@
+import os
+import time
+import random
 import tempfile
+import shutil
+import unittest
+import subprocess
 
 from pueuey import Queue, ConnAdapter, Worker
 from common import Notifier, ConnBaseTest
+import example_worker
 
+
+reg_args = None
 
 def register(*args):
     global reg_args
     reg_args = args
 
 class WorkerTest(ConnBaseTest):
-    def test_10_fetch_something(self):
+    q_name = 'test_worker'
+    concurrent_workers = 10
+    tasks = 100
+
+    def setUp(self):
+        super(WorkerTest, self).setUp()
+        self.working_directory = tempfile.mkdtemp()
+        self.running_workers = []
+
+    def _cleanup(self):
+        for process in self.running_workers:
+            process.kill()
+            process.wait()
+        shutil.rmtree(self.working_directory)
+        super(WorkerTest, self)._cleanup()
+
+    def _invoke_worker(self, fork=False):
+        p = subprocess.Popen(
+            [example_worker.__file__, self.working_directory,
+             '--dbname=' + self.dbname, '--queue=' + self.q_name] +
+            (['--forkworker'] if fork else []))
+        self.running_workers.append(p)
+        return p
+
+    def _check_exists(self, name, retry=2):
+        for i in range(1, retry + 1):
+            if os.path.exists(os.path.join(self.working_directory, name)):
+                break
+            time.sleep(i)
+        else:
+            self.fail("file `%s' does not exists!" % name)
+
+    def test_00_fetch_something(self):
         global reg_args
         worker = Worker(connection=self._connect(), q_name=self.q_name)
         self.queue.enqueue("test_30_worker.register", ["foo", "bar"])
-        worker.work()
-        self.assertEqual(reg_args, ("foo", "bar"))
+        for i in range(5):
+            worker.work()
+            if reg_args == ("foo", "bar"):
+                break
+        else:
+            self.fail()
+
+    def test_10_one_worker_success(self):
+        self._invoke_worker()
+        self.queue.enqueue("example_worker.touch", ["foo"])
+        self._check_exists("foo")
+
+    @unittest.expectedFailure
+    def test_15_one_worker_failure(self):
+        self._invoke_worker()
+        self.queue.enqueue("example_worker.touch", ["foo"])
+        self._check_exists("bar")
+
+    def test_20_lot_of_workers_no_delay(self):
+        for i in range(self.concurrent_workers):
+            self._invoke_worker()
+        for i in range(self.tasks):
+            self.queue.enqueue("example_worker.touch", ["job_%03d" % i])
+        for i in range(self.tasks):
+            self._check_exists("job_%03d" % i)
+        self.assertEqual(self.queue.count(), 0)
+
+    def test_25_lot_of_workers_random_delay(self):
+        for i in range(self.concurrent_workers):
+            self._invoke_worker()
+        for i in range(self.tasks):
+            self.queue.enqueue("example_worker.touch",
+                ["job_%03d" % i, random.randint(0, 2)])
+        for i in range(self.tasks):
+            self._check_exists("job_%03d" % i, retry=5)
+        time.sleep(2) # wait the maximum delay to make sure all jobs have ended
+        self.assertEqual(self.queue.count(), 0)
+
+    def test_30_lot_of_workers_no_delay_using_fork(self):
+        for i in range(self.concurrent_workers):
+            self._invoke_worker(fork=True)
+        for i in range(self.tasks):
+            self.queue.enqueue("example_worker.touch", ["job_%03d" % i])
+        for i in range(self.tasks):
+            self._check_exists("job_%03d" % i)
+        self.assertEqual(self.queue.count(), 0)
+
+    def test_35_lot_of_workers_random_delay_using_fork(self):
+        for i in range(self.concurrent_workers):
+            self._invoke_worker(fork=True)
+        for i in range(self.tasks):
+            self.queue.enqueue("example_worker.touch",
+                ["job_%03d" % i, random.randint(0, 2)])
+        for i in range(self.tasks):
+            self._check_exists("job_%03d" % i, retry=5)
+        time.sleep(2) # wait the maximum delay to make sure all jobs have ended
+        self.assertEqual(self.queue.count(), 0)
