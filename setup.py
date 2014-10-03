b29a5306fccee5e2f5aa3e1e705afd069e01d082
Cécile Tonglet <cecile.tonglet@gmail.com>
2014-10-03 10:00:38 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "pueuey",
+    version = "1.0",
+    author = "Cecile Tonglet",
+    author_email = "cecile.tonglet@gmail.com",
+    description = ("queue_classic ported to Python 2: Simple, efficient "
+                   "worker queue for Ruby & PostgreSQL."),
+    license = "MIT",
+    keywords = "postgres queue worker ruby",
+    url = "https://github.com/cecton/pueuey",
+    packages = find_packages(),
+    install_requires = ['psycopg2'],
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Database",
+        "Topic :: System :: Distributed Computing",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
